927fd73309c1ff26585d0537e423fc18a66e9d29
aldi8477 <79303833+aldi8477@users.noreply.github.com>
2021-06-13 18:40:57 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,42 +1,16 @@
-<?php include 'config/koneksi.php'; ?>
-<html>
-    <head>
-        <title>CRUD BASIS DATA</title>
-    </head>
-    <body>
-        <a href="?halaman=dashboard">Dashboard</a> | 
-        <a href="?halaman=kategori"> Kategori </a> |
-        <a href="?halaman=kategori-prosedur"> Kategori Prosedur</a> |
-        <a href="?halaman=kategori-pdo"> Kategori PDO </a>
-        
-        <hr>
+<?php
 
-        <?php 
-            if (isset($_GET['halaman'])) {
-                if ($_GET['halaman'] == "dashboard") {
-                    include 'page/dashboard.php';
-                } else if ($_GET['halaman'] == "tambah-kategori") {
-                    include 'page/tambah-kategori.php';
-                } else if ($_GET['halaman'] == "kategori") {
-                    include 'page/kategori.php';
-                } else if ($_GET['halaman'] == "edit-kategori") {
-                    include 'page/edit-kategori.php';
-                } else if ($_GET['halaman'] == "kategori-prosedur") {
-                    include 'page/kategori-prosedur.php';
-                } else if ($_GET['halaman'] == "tambah-kategori-prosedur") {
-                    include 'page/tambah-kategori-prosedur.php';
-                } else if ($_GET['halaman'] == "edit-kategori-procedur") {
-                    include 'page/edit-kategori-prosedur.php';
-                } else if ($_GET['halaman'] == "kategori-pdo") {
-                    include 'page/kategori-pdo.php';
-                } else if ($_GET['halaman'] == "tambah-kategori-pdo") {
-                    include 'page/tambah-kategori-pdo.php';
-                } else if ($_GET['halaman'] == "edit-pdo") {
-                    include 'page/edit-kategori-pdo.php';
-                }
-            } else {
-                include "page/dashboard.php";
-            }
-        ?>
-    </body>
-</html>
+error_reporting(0);
+session_start();
+
+include_once 'dbconection.php';
+//sertakan atau panggil potongan potongan layout web
+include_once 'ar_menu.php';
+include_once 'header.php';
+include_once 'menu.php';
+include_once 'sidebar.php';
+include_once 'main.php';
+include_once 'footer.php';
+
+?>
+ 
