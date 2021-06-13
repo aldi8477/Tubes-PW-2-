959610f40dbbc96975931a3164707b151a759aa9
aldi8477 <79303833+aldi8477@users.noreply.github.com>
2021-06-13 18:41:21 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,19 @@
+<h1>Welcome To My Store</h1>
+<div id="slider-wrapper">
+    <div id="slider" class="nivoSlider">
+        <img src="images/slider/02.jpg" alt=""/>
+        <img src="images/slider/01.jpg" alt=""/>
+        <img src="images/slider/03.jpg" alt=""/>
+        <img src="images/slider/04.jpg" alt=""/>
+    </div>
+    <div id="htmlcaption" class="nivo-html-caption">
+        <strong>Ini</strong> adalah contoh <em>HTML</em> teks dengan  <a href="#">sebuah link</a>.
+    </div>
+</div>
+<script type="text/javascript" src="js/jquery-1.4.3.min.js"></script>
+<script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+    $('#slider').nivoSlider();
+});
+</script>
