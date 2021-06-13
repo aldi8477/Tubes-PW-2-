1bded352ea7fa22d7838503800d085839dee4d6b
aldi8477 <79303833+aldi8477@users.noreply.github.com>
2021-06-13 18:43:31 +0700

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,42 @@
+<h1>Checkout</h1>
+<h5><strong>INFORMASI TAGIHAN</strong></h5>
+<div class="content_half float_l checkout">
+    NAMA:  
+    <input type="text"  style="width:300px;"  />
+    <br />
+    <br />
+    Alamat:
+                    <input type="text"  style="width:300px;"  />
+    <br />
+    <br />
+    Kota:
+    <input type="text"  style="width:300px;"  />
+    <br />
+    <br />
+</div>
+
+<div class="content_half float_r checkout">
+    E-MAIL
+                    <input type="text"  style="width:300px;"  />
+    <br />
+    <br />
+    No.HP<br />
+                    <span style="font-size:10px">Mohon sebutkan  nomer telepon anda yang dapat dihubungi.Anda mungkin diberi notifikasi untuk memverifikasi untuk menyelesaikan pesanan.</span>
+    <input type="text"  style="width:300px;"  />
+</div>
+
+<div class="cleaner h50"></div>
+<h3>Keranjang Pembelian</h3>
+<h4>TOTAL JUMLAH:225.000.00<a href="index.php?hal=products"> </h4>
+<a href="http://localhost/shoes-store-master/invoice/" class="btn btn-primary">Bayar</a> 
+            <p><input type="checkbox" />
+            Saya menerima <a href="#">syarat pengguna</a> Dari situs ini.</p>
+<table style="border:1px solid #CCCCCC;" width="100%">
+    <tr>
+        <td height="80px"> <img src="images/gopay.png" alt="gopay" /></td>
+        <td width="400px;" style="padding: 0px 20px;">Disarankan jika anda memiliki akun gopay,waktu pemesanan lebih cepat.
+        </td>
+        <td><a href="#" class="more">GOPAY</a></td>
+    </tr>
+   
+</table>
