262db2fed15820fbd9f696f9dc26b9e84360f1e3
aldi8477 <79303833+aldi8477@users.noreply.github.com>
2021-06-13 18:42:54 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,35 @@
+    <h1>Hubungi kami</h1>
+    <div class="content_half float_l">
+        <p>Jika anda memiliki masalah atau pertanyaan, silahkan hubungi kami.</p>
+        <div id="contact_form">
+           <form method="post" name="contact" action="#">
+
+                <label for="author">Nama:</label> <input type="text" id="author" name="author" class="required input_field" />
+                <div class="cleaner h10"></div>
+                <label for="email">Email:</label> <input type="text" id="email" name="email" class="validate-email required input_field" />
+                <div class="cleaner h10"></div>
+
+                <label for="phone">No.HP:</label> <input type="text" name="phone" id="phone" class="input_field" />
+                <div class="cleaner h10"></div>
+
+                <label for="text">Pesan:</label> <textarea id="text" name="text" rows="0" cols="0" class="required"></textarea>
+                <div class="cleaner h10"></div>
+
+                <input type="submit" class="submit_btn" name="submit" id="submit" value="Send" />
+
+            </form>
+        </div>
+    </div>
+<div class="content_half float_r">
+        <h5>Our Office</h5>
+                Jl Raya Pamukulan RT 001/07 No. 26, <br />
+                Koleang,Jasinga<br />
+                Bogor<br /><br />
+
+                No.HP: 0815 4684 7732<br />
+                Email: <a href="aldinurhanudin08@gmail.com">aldinurhanudin08@gmail.com</a><br/>
+</div>
+
+<div class="cleaner h40"></div>
+<!--map pada peta-->
+<iframe width="680" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Central+Park,+New+York,+NY,+USA&amp;aq=0&amp;sll=14.093957,1.318359&amp;sspn=69.699334,135.263672&amp;vpsrc=6&amp;ie=UTF8&amp;hq=Central+Park,+New+York,+NY,+USA&amp;ll=40.778265,-73.96988&amp;spn=0.033797,0.06403&amp;t=m&amp;output=embed"></iframe>
