6a2e2b80565a90748fbc87a51f5af15f5a439db8
aldi8477 <79303833+aldi8477@users.noreply.github.com>
2021-06-13 18:44:07 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,12 @@
+<h1>Tentang Kita</h1>
+<h2>Latar belakang Perusahaan</h2>
+<p>Alas kaki, khususnya sepatu memiliki tujuan tersendiri bagi para pemakainya,
+berbagai jenis dan model sepatu yang berbeda-beda sudah banyak dibuat dan
+dikembangkan. Tujuan pemakaian sepatu adalah untuk meningkatkan performa saat
+melakukan berbagai kegiatan, sebagai alat keselamatan, ataupun sebagai penunjang
+penampilan penggunanya, dengan tetap memasukkan unsur ergonomis pada kaki
+pemakainya. .</p>
+<div class="cleaner h20"></div>
+<h3>Tim Manajemen</h3>
+        <p>Zayn Hiro Malik Coorporation.</p>
+<div class="cleaner"></div>
