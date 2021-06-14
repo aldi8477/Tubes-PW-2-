5a8769e94a2b16dae1f2f05085ddbf04dece80b2
aldi8477 <79303833+aldi8477@users.noreply.github.com>
2021-06-14 16:13:00 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,29 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "db_tokosepatu_aldi";
+$koneksi = new mysqli ("localhost", "root", "", "db_tokosepatu");
 
-// Create connection
-$mysqli_object = new mysqli($servername, $username, $password, $database);
-$mysqli_procedur = mysqli_connect($servername, $username, $password, $database);
-
-
-// Check connection
-if ($mysqli_object->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$koneksi) {
+    die("Connection Failed");
 }
-
-if (!$mysqli_procedur) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-try {
-    $pdo = new PDO("mysql:host=$servername;dbname=db_tokosepatu_aldi", $username, $password);
-    // set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
-
-?>
